6341fe5735b3b2e2fbe0bd7982a0f1ade40b4106
unknown <nwessel429@gmail.com>
2019-11-20 14:51:38 -0600

    finished all practises for it.

---
+++ Customers.cs
@@ -0,0 +1,11 @@
+// Build a collection of customers who are millionaires
+namespace LINQedList
+{
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+}
